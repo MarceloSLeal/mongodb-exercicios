7650621a93a3dff0ebbf7a9c4f687412d2c5eef7
MarceloSLeal <marcelosleal@outlook.com>
2023-08-18 01:25:41 -0300

    Mais um insert

---
+++ outroInsert.js
@@ -0,0 +1,12 @@
+db.estados.find.pretty()
+
+db.empresas.insertOne({
+    nome: "Bradesco",
+    estadoId: ObjectId("64deeef7247313bdb2121ec7")
+})
+
+db.empresas.insertOne({
+    nome: "Vale",
+    cidade_id: ObjectId("64deeef7247313bdb2121ec6")
+
+})
